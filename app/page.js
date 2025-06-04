35a2886bf973aa3da6c67f894b93602f4912cda0
abhishek-rautela <abhisheksinghrautela18@gmail.com>
2025-06-04 12:51:08 +0530

    sucessfully imported next js tailwind css and schadn ui library in the project and tested it with demo text

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div>
+      <h2>Abhishek Singh Rautela</h2>
+    <Button>
+      Hello World
+    </Button>
+    </div>
+  );
+}
